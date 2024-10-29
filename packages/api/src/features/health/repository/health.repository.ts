eda5876f23571df22b170b575c98131212166c59
Louisan Tchitoula <t.louisan@outlook.fr>
2024-10-30 00:00:20 +0100

    fix: add db connection to health repository example

--- packages/api/src/features/health/repository/health.repository.ts
+++ packages/api/src/features/health/repository/health.repository.ts
@@ -1,7 +1,18 @@
+import { inject, injectable } from "tsyringe";
 import { IHealthRepository } from "./health.repository.interface";
+import { Knex } from "knex";
 
+@injectable()
 export class HealthRepository implements IHealthRepository {
-  getHealth(): Promise<boolean> {
-    return Promise.resolve(false);
+  constructor(@inject("Database") private readonly db: Knex) {}
+
+  async getHealth(): Promise<boolean> {
+    try {
+      await this.db.raw("SELECT 1");
+      return true;
+    } catch (error) {
+      console.error("Database connection failed:", error);
+      return false;
+    }
   }
 }
