e0a4e07ac27e35aa7a327126c7ba896c47aaf833
Louisan Tchitoula <t.louisan@outlook.fr>
2024-10-31 21:13:02 +0100

    feat: add link route to app

--- packages/api/src/common/presentation/routes.ts
+++ packages/api/src/common/presentation/routes.ts
@@ -2,5 +2,6 @@ import AuthRoute from "../../features/auth";
 import FileRoute from "../../features/files";
 import HealthRoute from "../../features/health";
 import UserRoute from "../../features/user";
+import LinkRoute from "../../features/links";
 
-export const routes = [HealthRoute, AuthRoute, FileRoute, UserRoute];
+export const routes = [HealthRoute, AuthRoute, FileRoute, LinkRoute, UserRoute];
