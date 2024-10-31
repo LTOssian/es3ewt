299bd22ae6322c27b41fb622761e93c3645a33e7
Louisan Tchitoula <t.louisan@outlook.fr>
2024-10-31 13:55:55 +0100

    feat: add file core entity

--- packages/core/file/file.error.ts
+++ packages/core/file/file.error.ts
@@ -6,3 +6,12 @@ export class BadFileRequestError extends Error {
     this.name = this.constructor.name;
   }
 }
+
+export class FileNotFoundError extends Error {
+  public readonly statusCode: number = 404;
+
+  constructor(credentials?: { message?: string }) {
+    super(credentials?.message || "File not found");
+    this.name = this.constructor.name;
+  }
+}
---
+++ packages/core/file/file.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const getFileByIdRequestSchema = z.object({
+  fileId: z.string().min(1),
+});
+
+export const fileResponseSchema = z.object({
+  id: z.string().min(1),
+  path: z.string().min(1),
+  name: z.string().min(1),
+  user_id: z.string().min(1),
+  size: z.number(),
+  lastUpdate: z.date(),
+});
+
+export type TGetFileByIdRequest = z.infer<typeof getFileByIdRequestSchema>;
+// export type TGetFileByIdResponse = z.infer<typeof getFileByIdResponseSchema>;
+export type TFileResponse = z.infer<typeof fileResponseSchema>;
