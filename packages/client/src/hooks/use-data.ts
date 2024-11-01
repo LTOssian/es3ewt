deb5aef394b1f7b8425ee289bcee255c4d06f33c
Louisan Tchitoula <t.louisan@outlook.fr>
2024-11-01 10:11:31 +0100

    feat: add data fetching hook and helper lib methods

---
+++ packages/client/src/hooks/use-data.ts
@@ -0,0 +1,46 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  authorizedGet,
+  authorizedPost,
+  authorizedPut,
+  authorizedDelete,
+} from "../lib/authorized-fetch";
+
+// Use a query to GET data
+export function useGetData(path: string, options?: any) {
+  return useQuery({
+    queryKey: [path],
+    queryFn: () => authorizedGet(path),
+    ...options,
+  });
+}
+
+// Use a mutation to POST data
+export function usePostData<T>(path: string, options?: any) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (body: T) => authorizedPost(path, body).then((res) => res.data),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [path] }),
+    ...options,
+  });
+}
+
+// Use a mutation to PUT data
+export function usePutData<T>(path: string, options?: any) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (body: T) => authorizedPut(path, body).then((res) => res.data),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [path] }),
+    ...options,
+  });
+}
+
+// Use a mutation to DELETE data
+export function useDeleteData(path: string, options?: any) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: () => authorizedDelete(path).then((res) => res.data),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [path] }),
+    ...options,
+  });
+}
---
+++ packages/client/src/lib/authorized-fetch.ts
@@ -0,0 +1,52 @@
+async function makeUrl(path: string) {
+  const slashlessPath = path.startsWith("/") ? path.slice(1) : path;
+  //   const domainResponse = await fetch("/api-info.json");
+  //   const { domain } = await domainResponse.json();
+  //   const domainSlash = domain.endsWith("/") ? domain : `${domain}/`;
+  //   return `${domainSlash}${slashlessPath}`;
+  return `http://localhost:8080/${slashlessPath}`; // TODO: use env variables
+}
+
+async function authorizedFetch(
+  path: string,
+  options: RequestInit = {},
+): Promise<{ data: any; status: number }> {
+  const url = await makeUrl(path);
+  const response = await fetch(url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    credentials: "include",
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+// CRUD operations
+
+export async function authorizedGet(path: string) {
+  return authorizedFetch(path);
+}
+
+export async function authorizedPost<T>(path: string, body: T) {
+  return authorizedFetch(path, {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
+}
+
+export async function authorizedPut<T>(path: string, body: T) {
+  return authorizedFetch(path, {
+    method: "PUT",
+    body: JSON.stringify(body),
+  });
+}
+
+export async function authorizedDelete(path: string) {
+  return authorizedFetch(path, {
+    method: "DELETE",
+  });
+}
